74804011e4c5f1025956c6432815b0bf5410eda9
kelvinzap <chisom859@gmail.com>
2022-03-18 13:30:52 +0100

    Section 2 Exercise completed

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+
+            return View(customers);
+        }
+
+        private List<Customer> GetCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer(){Id = 1, Name = "John Smith"},
+                new Customer(){Id = 2, Name = "Mary Williamo"}
+            };
+        }
+
+        //customer/details
+        [HttpGet]
+        public ActionResult Details(int Id)
+        {
+            if (Id == 0)
+                return HttpNotFound();
+
+            var customer = GetCustomers().SingleOrDefault(p => p.Id == Id);
+
+
+            return View(customer);
+        }
+    }
+}
